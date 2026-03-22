94900752fee9f7cc792ad0ccee19fd6d5e3ba99e
sohum-arora <sohumarora28@gmail.com>
2026-03-22 07:25:38 +0530

    Update PinpointLocalizer.kt

--- apexpathing/src/main/java/com/apexpathing/localization/PinpointLocalizer.kt
+++ apexpathing/src/main/java/com/apexpathing/localization/PinpointLocalizer.kt
@@ -10,6 +10,7 @@ import com.apexpathing.geometry.Pose2d
 
 /**
  * GoBilda Pinpoint odometry localizer.
+ * @Author Sohum Arora 22985
  */
 class PinpointLocalizer(
     private val hardwareMap: HardwareMap,
