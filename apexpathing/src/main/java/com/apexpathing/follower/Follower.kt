c0d1f85758911ea70b2033bf340c784c97d2e2bf
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2026-03-22 21:43:44 -0400

    Adding Follower

--- apexpathing/src/main/java/com/apexpathing/follower/Follower.kt
+++ apexpathing/src/main/java/com/apexpathing/follower/Follower.kt
@@ -1,4 +1,14 @@
 package com.apexpathing.follower
 
-class Follower {
+import com.apexpathing.util.math.Pose
+
+abstract class Follower {
+    abstract var currentPath: Path
+    private var currentT = 0.0
+
+    open var isFinished = false
+
+    abstract fun setTarget(target: Pose)
+
+    abstract fun update(currentPose: Pose)
 }
